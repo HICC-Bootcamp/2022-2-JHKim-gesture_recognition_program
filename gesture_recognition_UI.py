c76e0410ba6c169f31f757f2ebad39551c75b714
jinho_kim98 <rlawlsgh1227@gmail.com>
2022-02-18 21:14:19 +0900

    UI 디자인 구성하기 완료, UI 버튼 기능 연결(back 연동x)

--- gesture_recognition_UI.py
+++ gesture_recognition_UI.py
@@ -1,14 +1,15 @@
 import sys
-from PyQt5 import uic
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import *
 
-from PyQt5 import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 #초기에 생성하는 제스처의 수
 NumofGesture = 2
+
+#나중에 추가할 때 저장하고자 하는 곳을 기억하는 변수
+add_id = 0
 
 #initUI: 등록할 제스처의 수를 입력받는 UI
 class Gesture_recognition(QMainWindow):
@@ -41,9 +42,9 @@ class addDataset_UI(QMainWindow):
 
         self.current = 0    #현재 반복 수
         #확인 버튼을 누르면 이름과 기능을 저장한 후 영상을 녹화한다.
-        self.okayButton2.clicked.connect(self.okayButton2Clicked)
+        self.okayButton.clicked.connect(self.okayButtonClicked)
 
-    def okayButton2Clicked(self):
+    def okayButtonClicked(self):
         # 현재 반복 횟수가 초기에 설정한 제스처 수와 같아질 때 저장을 멈추고 학습화면으로 넘어간다.
         if(self.current >= NumofGesture):
             progress = progressbar_UI()
@@ -62,6 +63,26 @@ class progressbar_UI(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("ui/progressbar.ui", self)
+
+        self.progressBar.setStyleSheet("QProgressBar{\n"
+                                       "    background-color: rgb(255, 255, 255);\n"
+                                       "    color:rgb(0,0,0);\n"
+                                       "    border-style: none;\n"
+                                       "    border-bottom-right-radius: 10px;\n"
+                                       "    border-bottom-left-radius: 10px;\n"
+                                       "    border-top-right-radius: 10px;\n"
+                                       "    border-top-left-radius: 10px;\n"
+                                       "    text-align: center;\n"
+                                       "}\n"
+                                       "QProgressBar::chunk{\n"
+                                       "    border-bottom-right-radius: 10px;\n"
+                                       "    border-bottom-left-radius: 10px;\n"
+                                       "    border-top-right-radius: 10px;\n"
+                                       "    border-top-left-radius: 10px;\n"
+                                       "    background-color: rgb(175, 152, 216);\n"
+                                       "}\n"
+                                       "\n"
+                                       "")
 
         #100%가 되기 전까지 확인버튼 비활성화
         self.okayButton.setEnabled(False)
@@ -113,20 +134,82 @@ class ModifyUI(QMainWindow):
         super().__init__()
         loadUi("ui/ModifyUI.ui", self)
 
-        self.modifyButtonsGroup = QButtonGroup(self)
-        for button in self.modifyButtonsGroup.findChildren(QAbstractButton):
-            self.modifyButtonsGroup.button.clicked.connect(self.saveButtonClicked(button))
+        self.nameList = [self.name_1, self.name_2, self.name_3, self.name_4, self.name_5, self.name_6, self.name_7, self.name_8]
+        self.functionList = [self.comboBox_function_1, self.comboBox_function_2, self.comboBox_function_3, \
+                             self.comboBox_function_4, self.comboBox_function_5, self.comboBox_function_6, \
+                             self.comboBox_function_7, self.comboBox_function_8]
+
+        self.plus_1.clicked.connect(lambda: self.plusButtonClicked(1))
+        self.plus_2.clicked.connect(lambda: self.plusButtonClicked(2))
+        self.plus_3.clicked.connect(lambda: self.plusButtonClicked(3))
+        self.plus_4.clicked.connect(lambda: self.plusButtonClicked(4))
+        self.plus_5.clicked.connect(lambda: self.plusButtonClicked(5))
+        self.plus_6.clicked.connect(lambda: self.plusButtonClicked(6))
+        self.plus_7.clicked.connect(lambda: self.plusButtonClicked(7))
+        self.plus_8.clicked.connect(lambda: self.plusButtonClicked(8))
+
+        self.delete_1.clicked.connect(lambda: self.deleteButtonClicked(1))
+        self.delete_2.clicked.connect(lambda: self.deleteButtonClicked(2))
+        self.delete_3.clicked.connect(lambda: self.deleteButtonClicked(3))
+        self.delete_4.clicked.connect(lambda: self.deleteButtonClicked(4))
+        self.delete_5.clicked.connect(lambda: self.deleteButtonClicked(5))
+        self.delete_6.clicked.connect(lambda: self.deleteButtonClicked(6))
+        self.delete_7.clicked.connect(lambda: self.deleteButtonClicked(7))
+        self.delete_8.clicked.connect(lambda: self.deleteButtonClicked(8))
+
+        self.save_1.clicked.connect(lambda: self.saveButtonClicked(1))
+        self.save_2.clicked.connect(lambda: self.saveButtonClicked(2))
+        self.save_3.clicked.connect(lambda: self.saveButtonClicked(3))
+        self.save_4.clicked.connect(lambda: self.saveButtonClicked(4))
+        self.save_5.clicked.connect(lambda: self.saveButtonClicked(5))
+        self.save_6.clicked.connect(lambda: self.saveButtonClicked(6))
+        self.save_7.clicked.connect(lambda: self.saveButtonClicked(7))
+        self.save_8.clicked.connect(lambda: self.saveButtonClicked(8))
 
         self.okayButton.clicked.connect(self.okayButtonClicked)
 
-    def deleteButtonClicked(self, i):
-        pass
+    def plusButtonClicked(self, id):
+        global add_id
+        add_id = id - 1
+        self.adddata_after = addDataset_after()
+        self.adddata_after.show()
 
-    def saveButtonClicked(self):
-        print(0)
+    def deleteButtonClicked(self, id):
+        name = self.nameList[id-1].text()
+        func = self.functionList[id-1].currentText()
+        print(name)
+        print(func)
+
+    def saveButtonClicked(self, id):
+        name = self.nameList[id - 1].text()
+        func = self.functionList[id - 1].currentText()
+        print(name)
+        print(func)
+
+    # 데이터 셋 변동이 있으면 학습화면으로 아니면 메인 화면으로 간다.
+    def okayButtonClicked(self):
+        if(False):
+           widget.setCurrentIndex(widget.currentIndex() - 2)
+        else:
+            widget.setCurrentIndex(widget.currentIndex() - 1)
+
+
+class addDataset_after(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loadUi("ui/addDataset.ui", self)
+
+        self.okayButton.clicked.connect(self.okayButtonClicked)
 
     def okayButtonClicked(self):
-        widget.setCurrentIndex(widget.currentIndex() - 1)
+        name = self.lineEdit_name.text()
+        function = self.comboBox_function.currentText()
+        print(add_id)
+        print(name)
+        print(function)
+        #영상 녹화 후 팝업 창 종료
+        self.close()
+
 
 
 if __name__ == "__main__":
