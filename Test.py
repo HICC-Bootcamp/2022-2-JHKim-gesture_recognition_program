602da65fdd1435e5771e3c36c9593319111d4af1
darkenergy814 <boy814@naver.com>
2022-02-25 21:46:22 +0900

    testfile

--- Test.py
+++ Test.py
@@ -6,7 +6,10 @@ from back import *
 #addInformation('이름1','몰라')
 #deleteDatasetNameFunction('이름1','몰라')
 #deleteVideo('이름1')
-
+#addInformation('이름2','기능2')
+#changeNameFunction(0,'이름1','기능1')
+addInformation('이름1','기능2')
+changeModel(0,'이름1','이름2','기능1')
 
 
 
